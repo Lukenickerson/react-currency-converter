4f4c3fc72991073b8befbe46c047389de47d5ec8
Luke <luke.nickerson@gmail.com>
2020-08-13 23:05:52 -0400

    Updates

---
+++ precache-manifest.0acc2b4bf9dd1cd53d95a8b0b4777e0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0221858b39e9bdd67b5e0d2139110360",
+    "url": "/react-currency-converter/index.html"
+  },
+  {
+    "revision": "19e1128c3e9cb4c09c34",
+    "url": "/react-currency-converter/static/css/main.eb0e282b.chunk.css"
+  },
+  {
+    "revision": "dc9951c0bb98e9a94d32",
+    "url": "/react-currency-converter/static/js/2.34994746.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-currency-converter/static/js/2.34994746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e1128c3e9cb4c09c34",
+    "url": "/react-currency-converter/static/js/main.3dc17685.chunk.js"
+  },
+  {
+    "revision": "383061d652ddffaff869",
+    "url": "/react-currency-converter/static/js/runtime-main.fe2c33c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-currency-converter/static/media/logo.5d5d9eef.svg"
+  }
+]);
